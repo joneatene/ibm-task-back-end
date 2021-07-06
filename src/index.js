6cffe49b5cff38818b1dceb4a7423af73a9d47d1
joneatene <joneatene@gmail.com>
2021-07-06 13:10:03 +0300

    search logging

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const Joi = require("joi");
+const logger = require("../config/logger");
 require("dotenv").config();
 
 const app = express();
-app.use(express());
+app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -13,16 +14,29 @@ app.get("/", (req, res) => {
 
 app.post("/search", async (req, res) => {
   if (!req.body.keywords) {
-    return res.status(400).send({ error: "Incorrect data passed" });
+    return res.status(400).send({ error: "Invalid data passed" });
+  }
+  try {
+    const schema = Joi.array().max(40);
+    const keyworsValidation = schema.validate(req.body.keywords);
+    if (keyworsValidation.error) {
+      res.status(400).send({ error: "Invalid data passed" });
+    }
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .send({ error: "An error occured. Please try again later" });
   }
 
   try {
-    const schema = Joi.array().max(40);
-    const keywordsValidation = schema.validate($req.body.keywords);
+    await logger.log({
+      level: "info",
+      name: "search",
+      searchKeywords: `${req.body.keywords}`,
+    });
 
-    if (keywordsValidation.error) {
-      return res.status(400).send({ error: "Incorrect data passed" });
-    }
+    return res.send({ status: "Search keywords logged" });
   } catch (err) {
     console.log(err);
     return res
