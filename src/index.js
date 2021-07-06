30a9a6167af9d3806f852f8017353754f9a8e065
joneatene <joneatene@gmail.com>
2021-07-06 13:18:58 +0300

    articles info logging

--- src/index.js
+++ src/index.js
@@ -45,5 +45,48 @@ app.post("/search", async (req, res) => {
   }
 });
 
+app.post("/articles", async (req, res) => {
+  if (
+    !req.body.title ||
+    !req.body.image ||
+    !req.body.description ||
+    !req.body.url ||
+    !req.body.date
+  ) {
+    return res.status(400).send({ error: "Invalid data passed" });
+  }
+  try {
+    const schema = Joi.object();
+    const articleInfoValidation = schema.validate(req.body);
+    if (articleInfoValidation.error) {
+      res.status(400).send({ error: "Invalid data passed" });
+    }
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .send({ error: "An error occured. Please try again later" });
+  }
+
+  try {
+    await logger.log({
+      level: "info",
+      name: "articles",
+      articleInfo: `title: ${req.body.title}, image: ${req.body.image}, url: ${req.body.url}, description: ${req.body.description}, date: ${req.body.date}`,
+    });
+
+    return res.send({ status: "Article info logged" });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .send({ error: "An error occured. Please try again later" });
+  }
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on ${port} port`));
